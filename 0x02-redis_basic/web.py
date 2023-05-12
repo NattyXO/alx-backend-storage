c1b895d1e28457178ff1cf80580a8d846a79d96c
Natnael Bizuneh <106749223+NattyXO@users.noreply.github.com>
2023-05-12 21:48:01 +0300

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,21 +1,36 @@
 #!/usr/bin/env python3
 """
-create a web cach
+create a web cache
 """
 import redis
 import requests
-rc = redis.Redis()
-count = 0
 
+rc = redis.Redis()
 
 def get_page(url: str) -> str:
-    """ get a page and cach value"""
-    rc.set(f"cached:{url}", count)
-    resp = requests.get(url)
-    rc.incr(f"count:{url}")
-    rc.setex(f"cached:{url}", 10, rc.get(f"cached:{url}"))
-    return resp.text
-
+    """
+    Retrieves the HTML content of a URL using requests and caches the result
+    with an expiration time of 10 seconds. Tracks how many times the URL
+    has been accessed using Redis.
+    """
+    count_key = f"count:{url}"
+    cached_key = f"cached:{url}"
+    count = rc.incr(count_key)
+    if count == 1:
+        # If this is the first time accessing the URL, cache it for 10 seconds
+        resp = requests.get(url)
+        rc.setex(cached_key, 10, resp.text)
+    else:
+        # If the URL has been accessed before, retrieve the cached value
+        resp = rc.get(cached_key)
+        if resp is None:
+            # If the cached value has expired or been evicted, fetch the URL again
+            resp = requests.get(url)
+            rc.setex(cached_key, 10, resp.text)
+        else:
+            # If the cached value is still valid, return it
+            resp = resp.decode()
+    return resp
 
 if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+    print(get_page('http://slowwly.robertomurray.co.uk/delay/5000/url/http://www.google.com'))
